85147c2f463351efb7f7dc46a35956c9f1267ed3
NANI <NANI@DESKTOP-M44748F>
2021-03-29 11:29:05 +0530

    Hello World application First commit

---
+++ Helloworld Application/src/com/sr/intro1/Helloapp1.java
@@ -0,0 +1,11 @@
+package com.sr.intro1;
+
+public class Helloapp1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Welcome to java");
+
+	}
+
+}
